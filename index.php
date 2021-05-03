cf94ed56312ced1b043fef4f6cd82b25b7b0986b
SidMorad <SidMorad@users.noreply.github.com>
2021-05-03 13:23:42 +0000

    Ayria Payment Gateway-1.0.0 WooCommerce Wordpress plugin

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+/*
+Plugin Name: درگاه پرداخت آیریا
+Version: 1.0.0
+Description: افزونه درگاه پرداخت آیریا برای فروشگاه ساز وردپرس
+Plugin URI: https://ayriaplatform.com
+Author: Ariya Rashidi
+Author URI: http://rashidi.id.ir
+*/
+include_once("class-wc-gateway-ariyaclub.php");
